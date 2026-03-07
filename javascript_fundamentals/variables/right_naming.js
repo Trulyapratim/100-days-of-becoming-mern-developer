56c0bbdab28f71fb434c3bb507d9274fd439dcd2
Aman Prakash Sharma <amanprakashsharma@Amans-MacBook-Air-3.local>
2026-03-08 01:00:02 +0530

    added new folders and files

---
+++ javascript_fundamentals/variables/right_naming.js
@@ -0,0 +1,7 @@
+let planetEarth; // task was to create a right name of the variable
+let currentUser;
+
+
+// the solution present was and i believe it is better also.
+let outPlanetName = "Earth";
+let currentUserName = "John";
